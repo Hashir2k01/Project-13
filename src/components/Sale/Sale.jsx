b27d4895fd8e8225822497a44aa452d277d47004
Hashir2k01 <muhammad.hashir2k01@gmail.com>
2024-10-02 18:36:28 +0500

    first commit

---
+++ src/components/Sale/Sale.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import "./Sale.css";
+
+const Sale = () => {
+  return (
+    <>
+      <div className="first-section">
+        <h1>Black</h1>
+        <h2>Friday</h2>
+        <h3>Up to 70% on all Products</h3>
+      </div>
+
+      <div class="banner">
+        <div class="text-content">
+          <h2>LAST CHANCE</h2>
+          <h1>70%</h1>
+          <h2>OF ALL</h2>
+          <p>
+            SAMPLE TEXT. CLICK TO SELECT THE TEXT BOX. CLICK AGAIN OR br DOUBLE
+            CLICK TO START EDITING THE TEXT.
+          </p>
+          <button>START NOW</button>
+        </div>
+        <div class="image-content">
+          <img
+            src="https://images03.nicepage.com/a1389d7bc73adea1e1c1fb7e/3665e87a84a550e48420b0c1/11.png
+"
+            alt="Discount"
+          />
+        </div>
+      </div>
+
+      <div className="third-section">
+        <div className="text">
+          <h1>01</h1>
+          <h2>
+            AWESOME <br /> INTERIORS
+          </h2>
+          <p>
+            Paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Curabitur id suscipit ex. Suspendisse rhoncus laoreet purus quis
+            elementum. Phasellus sed efficitur dolor, et ultricies sapien.
+            Quisque fringilla sit amet dolor commodo efficitur. Aliquam et sem
+            odio. In ullamcorper nisi nunc, et molestie ipsum iaculis sit amet.
+          </p>
+        </div>
+
+        <div className="text">
+          <h1>02</h1>
+          <h2>
+            FRESH AND <br />
+            CREATIVE
+          </h2>
+          <p>
+            Paragraph. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Curabitur id suscipit ex. Suspendisse rhoncus laoreet purus quis
+            elementum. Phasellus sed efficitur dolor, et ultricies sapien.
+            Quisque fringilla sit amet dolor commodo efficitur. Aliquam et sem
+            odio. In ullamcorper nisi nunc, et molestie ipsum iaculis sit amet.
+          </p>
+        </div>
+      </div>
+
+      <div className="laptop-section">
+        <div>
+          <h1>
+            INSPIRED DESIGN. <br />
+            UNPARALLELED EXPERIENCE. <br />
+            EXEMPLARY SERVICE.
+          </h1>
+        </div>
+      </div>
+
+      <div className="social-section">
+        <div>
+          <h1>FOLLOW US</h1>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+          </svg>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64h98.2V334.2H109.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H255V480H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64z" />
+          </svg>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M384 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64h72.6l-2.2-.8c-5.4-48.1-3.1-57.5 15.7-134.7c3.9-16 8.5-35 13.9-57.9c0 0-7.3-14.8-7.3-36.5c0-70.7 75.5-78 75.5-25c0 13.5-5.4 31.1-11.2 49.8c-3.3 10.6-6.6 21.5-9.1 32c-5.7 24.5 12.3 44.4 36.4 44.4c43.7 0 77.2-46 77.2-112.4c0-58.8-42.3-99.9-102.6-99.9C153 139 112 191.4 112 245.6c0 21.1 8.2 43.7 18.3 56c2 2.4 2.3 4.5 1.7 7c-1.1 4.7-3.1 12.9-4.7 19.2c-1 4-1.8 7.3-2.1 8.6c-1.1 4.5-3.5 5.5-8.2 3.3c-30.6-14.3-49.8-59.1-49.8-95.1C67.2 167.1 123.4 96 229.4 96c85.2 0 151.4 60.7 151.4 141.8c0 84.6-53.3 152.7-127.4 152.7c-24.9 0-48.3-12.9-56.3-28.2c0 0-12.3 46.9-15.3 58.4c-5 19.3-17.6 42.9-27.4 59.3H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64z" />
+          </svg>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+          </svg>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+            <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zM351.3 199.3v0c0 86.7-66 186.6-186.6 186.6c-37.2 0-71.7-10.8-100.7-29.4c5.3 .6 10.4 .8 15.8 .8c30.7 0 58.9-10.4 81.4-28c-28.8-.6-53-19.5-61.3-45.5c10.1 1.5 19.2 1.5 29.6-1.2c-30-6.1-52.5-32.5-52.5-64.4v-.8c8.7 4.9 18.9 7.9 29.6 8.3c-9-6-16.4-14.1-21.5-23.6s-7.8-20.2-7.7-31c0-12.2 3.2-23.4 8.9-33.1c32.3 39.8 80.8 65.8 135.2 68.6c-9.3-44.5 24-80.6 64-80.6c18.9 0 35.9 7.9 47.9 20.7c14.8-2.8 29-8.3 41.6-15.8c-4.9 15.2-15.2 28-28.8 36.1c13.2-1.4 26-5.1 37.8-10.2c-8.9 13.1-20.1 24.7-32.9 34c.2 2.8 .2 5.7 .2 8.5z" />
+          </svg>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sale;
+
+
